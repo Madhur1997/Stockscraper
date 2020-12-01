d4d6154941184b96f3220c39984a4cd0a896bb74
Madhur batra <madhurbatra097@gmail.com>
2020-12-01 10:45:22 +0530

    Fixed clear script

--- clear.sh
+++ clear.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Clear out the running headless Chrome instances if you kill the program without chromedp getting a chance to clear up.
-ps -ef | grep "headless" | cut -d " " -f4 |xargs kill -9
+ps -ef | grep "headless" | awk '{print $2}' |xargs kill -9
